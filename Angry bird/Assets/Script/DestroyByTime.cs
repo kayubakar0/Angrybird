6e2043d8dfe621b593382e58d5acff2445f26b6a
kayubakar0 <suryaadrian970@gmail.com>
2021-09-22 18:15:39 +0700

    Game Angrybird

---
+++ Angry bird/Assets/Script/DestroyByTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float time;
+    float t = 0;    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (t < time)
+            t += Time.deltaTime;
+        else
+            Destroy(this.gameObject);
+    }
+}
---
+++ Angry bird/Assets/Script/Owl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owl : Bird
+{
+    public GameObject explosion;        
+
+    public void Explode()
+    {
+        Vector3 explosionPos = transform.position;
+        Instantiate(explosion, explosionPos, Quaternion.identity);
+        Destroy(this.gameObject);     
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Explode();
+    }
+
+}
